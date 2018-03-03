0a6a40ae2fa80c42d843a8e89a97e896bf76c90e
lenimbugua <leon9mbugua@gmail.com>
2018-03-03 23:48:11 +0300

    Functional Components

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square" onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
@@ -19,7 +17,7 @@ class Board extends React.Component {
       squares: Array(9).fill(null),
     };
   }
-  
+
   handleClick(i) {
     const squares = this.state.squares.slice();
     squares[i] = 'X';
